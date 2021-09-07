304ef7db65647009c8881de4d544776ab7f6199d
uedymd <ueda.shoesaz@gmail.com>
2021-09-07 20:00:36 +0900

    brand_setテーブルにカラムを追加

---
+++ database/migrations/2021_09_07_195635_add_column_brand_set_to_rakutens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnBrandSetToRakutensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rakutens', function (Blueprint $table) {
+            $table->char('brand_set_id', 255)->after('ng_keyword');
+            $table->char('rate_set_id', 255)->after('brand_set_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rakutens', function (Blueprint $table) {
+            $table->dropColumn('brand_set_id');
+            $table->dropColumn('rate_set_id');
+        });
+    }
+}
