23f9b30aa47b0197e895ea6981a91378c9143fd8
uedymd <ueda.shoesaz@gmail.com>
2021-09-10 02:47:55 +0900

    楽天商品表示UI調整

--- resources/views/rakuten/items.blade.php
+++ resources/views/rakuten/items.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-10xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-scroll shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200 overflow-x-scroll">
                     <table class="table-auto w-full mt-5 bg-white">
                         <caption class="bg-gray-100">
                             <div class="border px-4 py-2">{{$rakutebn_data->title}}</div>
