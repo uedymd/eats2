883913272d0e196328dd6d3dd484ecd866b8b9d3
uedymd <ueda.shoesaz@gmail.com>
2021-09-12 02:43:21 +0900

    楽天設定編集時のviewを修正

--- resources/views/rakuten/edit.blade.php
+++ resources/views/rakuten/edit.blade.php
@@ -11,41 +11,23 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                    <form method="POST" action="{{ route('rakuten.update',['id'=>$rakuten->id]) }}">
+                    <form method="POST" action="{{ route('rakuten.store') }}">
                         @csrf
 
                         <div class="mt-4">
-                            <div class="flex">
-                                <div>
-                                    <x-label for="title" :value="__('設定名')" />
-                                    <x-input id="title" class="block mt-1 w-full" type="text" name="title" value="{{$rakuten->title}}" required autofocus />
-                                </div>
-                            </div>
-                        </div>
+                            <div>
+                                <x-label for="title" :value="__('設定名')" />
 
-                        <div class="mt-4">
-                            <div class="flex">
-                                <div class="flex-grow">
-                                    <x-label for="keyword" :value="__('検索キーワード')" />
-
-                                    <x-input id="keyword" class="block mt-1 w-full" type="text" name="keyword" value="{{$rakuten->keyword}}" required autofocus />
-                                </div>
-                                <div class="flex-grow ml-5">
-                                    <x-label for="ng_keyword" :value="__('除外キーワード')" />
-
-                                    <x-input id="ng_keyword" class="block mt-1 w-full" type="text" name="ng_keyword" value="{{$rakuten->ng_keyword}}" autofocus />
-                                </div>
+                                <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title',$rakuten->title)" required autofocus />
                             </div>
                         </div>
 
                         <div class="mt-4 flex">
                             <div class="flex-shrink-0 w-8/12">
-                                <x-label for="genre" :value="__('ジャンル')" />
+                                <x-label for="keyword" :value="__('検索キーワード')" />
 
-                                <x-input id="genre" class="block mt-1 w-full" type="text" name="genre" value="{{$rakuten->genre}}" autofocus />
-                                <x-input id="genre_id" type="hidden" name="genre_id" value="{{$rakuten->genre_id}}" />
+                                <x-input id="keyword" class="block mt-1 w-full" type="text" name="keyword" :value="old('keyword',$rakuten->keyword)" required autofocus />
                             </div>
-
                             <div class="flex-shrink-0 w-3/12 ml-5">
                                 <x-label for="keyword" :value="__('対象ブランド')" />
                                 {{ Form::select(
@@ -55,11 +37,15 @@
                                     ['class'=>'block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500','required']
                                 ) }}
                             </div>
-
-
                         </div>
 
                         <div class="mt-4">
+                            <div class="flex-shrink-0 w-8/12">
+                                <x-label for="genre" :value="__('楽天ジャンル')" />
+
+                                <x-input id="genre" class="block mt-1 w-full" type="text" name="genre" :value="old('genre',$rakuten->genre)" autofocus />
+                                <x-input id="genre_id" type="hidden" name="genre_id" :value="old('genre_id',$rakuten->genre_id)" />
+                            </div>
                             <div class="block rakuten__genre--button">
                                 <ul class="rakuten__genre--selector">
                                     <li class="rakuten__genre--parent hasChild" data-genre="0"><span class="block p-3 mt-5 rounded bg-blue-200 text-center w-2/12">ジャンルを選択</span></li>
@@ -71,26 +57,27 @@
                             <div class="flex-1">
                                 <x-label for="ng_keyword" :value="__('除外キーワード')" />
 
-                                <x-input id="ng_keyword" class="block mt-1 w-10/12" type="text" name="ng_keyword" value="{{$rakuten->ng_keyword}}" autofocus />
+                                <x-input id="ng_keyword" class="block mt-1 w-10/12" type="text" name="ng_keyword" :value="old('ng_keyword',$rakuten->ng_keyword)" autofocus />
                             </div>
                             <div class="flex-1">
                                 <x-label for="ng_url" :value="__('除外URL')" />
 
-                                <x-input id="ng_url" class="block mt-1 w-10/12" type="text" name="ng_url" value="{{$rakuten->ng_url}}" autofocus />
+                                <x-input id="ng_url" class="block mt-1 w-10/12" type="text" name="ng_url" :value="old('ng_url',$rakuten->ng_url)" autofocus />
                             </div>
                         </div>
+
 
 
                         <div class="mt-4 flex">
                             <div class="flex-1">
                                 <x-label for="price_min" :value="__('最小価格')" />
 
-                                <x-input id="price_min" class="mt-1 w-10/12" type="number" name="price_min" value="{{$rakuten->price_min}}" autofocus />円
+                                <x-input id="price_min" class="mt-1 w-10/12" type="number" name="price_min" :value="old('price_min',$rakuten->price_min)" autofocus />円
                             </div>
                             <div class="flex-1">
                                 <x-label for="price_max" :value="__('最大価格')" />
 
-                                <x-input id="price_max" class="mt-1 w-10/12" type="number" name="price_max" value="{{$rakuten->price_max}}" autofocus />円
+                                <x-input id="price_max" class="mt-1 w-10/12" type="number" name="price_max" :value="old('price_max',$rakuten->price_max)" autofocus />円
                             </div>
                             <div class="flex-1">
                                 <x-label for="rate" :value="__('レート')" />
@@ -103,7 +90,7 @@
                                 ) }}
                             </div>
                         </div>
-
+                        
                         <div class="mt-4 flex">
                             <div class="flex-1">
                                 <x-label for="price_min" :value="__('ebayカテゴリー')" />
@@ -126,9 +113,9 @@
                                             ];
                                         }
                                     @endphp
-                                    {{ Form::radio('best_offer', '1',old('best_offer',$best_offer['best_offer_true']),['id'=>'best_offer_true'] );}}
+                                    {{ Form::radio('best_offer', '1',$best_offer['best_offer_true'],['id'=>'best_offer_true'] );}}
                                     {{Form::label('best_offer_true','有効',['class'=>'custom-control-label mr-10'])}}
-                                    {{ Form::radio('best_offer', '0',old('best_offer',$best_offer['best_offer_false']),['id'=>'best_offer_false'] );}}
+                                    {{ Form::radio('best_offer', '0',$best_offer['best_offer_false'],['id'=>'best_offer_false'] );}}
                                     {{Form::label('best_offer_false','無効',['class'=>'custom-control-labelx'])}}
                                 </div>
                             </div>
@@ -141,32 +128,24 @@
                                                 'condition_new' => true,
                                                 'condition_used' => false,
                                             ];
-                                        }elseif($rakuten->condition==2){
+                                        }elseif(!is_null($rakuten->condition)&&$rakuten->condition==0){
                                             $condition = [
                                                 'condition_new' => true,
                                                 'condition_used' => false,
                                             ];
                                         }
                                     @endphp
-                                    {{ Form::radio('condition', '1',old('condition',$condition['condition_new']),['id'=>'condition_new'] );}}
+                                    {{ Form::radio('condition', '1',$condition['condition_new'],['id'=>'condition_new'] );}}
                                     {{Form::label('condition_new','新品',['class'=>'custom-control-label mr-10'])}}
-                                    {{ Form::radio('condition', '2',old('condition',$condition['condition_used']),['id'=>'condition_used'] );}}
+                                    {{ Form::radio('condition', '2',$condition['condition_used'],['id'=>'condition_used'] );}}
                                     {{Form::label('condition_used','中古',['class'=>'custom-control-labelx'])}}
                                 </div>
                             </div>
                         </div>
 
-                        <div class="mt-4">
-                            <div>
-                                <label for="status">ステータス</label>
-                                {{ Form::select('status',$status_array,$rakuten->status) }}
-
-                            </div>
-                        </div>
-
                         <div class="flex items-center justify-end mt-4">
                             <x-button class="ml-4">
-                                {{ __('更新') }}
+                                {{ __('登録') }}
                             </x-button>
                         </div>
                     </form>
