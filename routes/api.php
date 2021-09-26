14ceea0fc9d14623a53752aa6cda31cb586527f2
uedymd <ueda.shoesaz@gmail.com>
2021-09-26 11:44:11 +0900

    update:ストックAPI実装

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Rakuten\RakutenItemController;
 use App\Http\Controllers\EbayItemController;
+use App\Http\Controllers\StocksController;
 
 /*
 |--------------------------------------------------------------------------
