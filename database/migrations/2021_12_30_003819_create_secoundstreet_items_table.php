a1017f5c9b77bea3a6fabb1e196b36a441074b87
上田宏 <ueda.shoesaz@gmail.com>
2022-02-06 19:46:38 +0900

    bugfix:secoundstreet migrate

--- database/migrations/2021_12_30_003819_create_secoundstreet_items_table.php
+++ database/migrations/2021_12_30_003819_create_secoundstreet_items_table.php
@@ -1,44 +1,44 @@
-<?php
+/php<?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateSecoundstreetItemsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateSecoundstreetItemsTable extends Migration
     {
-        Schema::create('secoundstreet_items', function (Blueprint $table) {
-            $table->id();
-            $table->bigInteger('scoundstreet_id');
-            $table->string('url', 255);
-            $table->string('jp_title', 255);
-            $table->longText('origin_title')->nullable();
-            $table->longText('jp_content')->nullable();
-            $table->longText('origin_content')->nullable();
-            $table->string('en_title', 255)->nullable();
-            $table->longText('en_content')->nullable();
-            $table->text('jp_brand')->nullable();
-            $table->text('en_brand')->nullable();
-            $table->char('price', 255);
-            $table->char('doller', 255)->nullable();
-            $table->longtext('images')->nullable();
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('secoundstreet_items', function (Blueprint $table) {
+                $table->id();
+                $table->bigInteger('secoundstreet_id');
+                $table->string('url', 255);
+                $table->string('jp_title', 255);
+                $table->longText('origin_title')->nullable();
+                $table->longText('jp_content')->nullable();
+                $table->longText('origin_content')->nullable();
+                $table->string('en_title', 255)->nullable();
+                $table->longText('en_content')->nullable();
+                $table->text('jp_brand')->nullable();
+                $table->text('en_brand')->nullable();
+                $table->char('price', 255);
+                $table->char('doller', 255)->nullable();
+                $table->longtext('images')->nullable();
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('secoundstreet_items');
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('secoundstreet_items');
+        }
     }
-}
