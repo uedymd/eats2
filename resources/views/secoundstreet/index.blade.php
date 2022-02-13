8ef870e7e4638f2e613cbd70c2e2325f926dfa68
上田宏 <ueda.shoesaz@gmail.com>
2022-02-13 16:58:13 +0900

    bugfix:セカンドストリートタイトル

--- resources/views/secoundstreet/index.blade.php
+++ resources/views/secoundstreet/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            secoundstreet設定
+            2nd STREET設定
         </h2>
     </x-slot>
 
