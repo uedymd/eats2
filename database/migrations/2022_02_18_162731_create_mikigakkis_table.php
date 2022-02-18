2c2272461ec1aa7aaeaaa87b38db80260c909bc2
上田宏 <ueda.shoesaz@gmail.com>
2022-02-18 17:09:20 +0900

    update:三木楽器

--- database/migrations/2022_02_18_162731_create_mikigakkis_table.php
+++ database/migrations/2022_02_18_162731_create_mikigakkis_table.php
@@ -15,6 +15,24 @@ class CreateMikigakkisTable extends Migration
     {
         Schema::create('mikigakkis', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('url');
+            $table->char('ebay_category', 255)->nullable();
+            $table->string('ng_keyword', 255)->nullable();
+            $table->char('brand_set_id', 255)->nullable();
+            $table->char('rate_set_id', 255)->nullable();
+            $table->string('ng_url', 255)->nullable();
+            $table->char('best_offer', 1)->nullable();
+            $table->char('condition', 1)->nullable();
+            $table->string('type', 255);
+            $table->text('sku')->nullable();
+            $table->text('payment_profile')->nullable();
+            $table->text('return_profile')->nullable();
+            $table->text('shipping_profile')->nullable();
+            $table->char('status', 1);
+            $table->char('template', 10);
+            $table->integer('priority')->nullable()->default(2);
+            $table->dateTime('checked_at')->nullable();
             $table->timestamps();
         });
     }
