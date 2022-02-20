56745932c2c00c2ea90563219f2e8d4de319aaaf
上田宏 <ueda.shoesaz@gmail.com>
2022-02-20 17:02:51 +0900

    update:スクレイピング詳細

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -73,16 +73,40 @@ class SettingController extends Controller
     {
         $count = $setting->where('site', $site)->count();
 
+        $titles = str_replace(array("\r\n", "\r", "\n"), "\n", $request->input('ng_title'));
+        $titles = explode("\n", $titles);
+        $titles = array_unique($titles);
+        array_multisort(array_map("mb_strlen", $titles), SORT_DESC, $titles);
+        $title = "";
+        foreach ($titles as $val) {
+            $title .= $val;
+            if ($val !== end($titles)) {
+                $title .= "\n";
+            }
+        }
+
+        $contents = str_replace(array("\r\n", "\r", "\n"), "\n", $request->input('ng_content'));
+        $contents = explode("\n", $contents);
+        $contents = array_unique($contents);
+        array_multisort(array_map("mb_strlen", $contents), SORT_DESC, $contents);
+        $content = "";
+        foreach ($contents as $val) {
+            $content .= $val;
+            if ($val !== end($contents)) {
+                $content .= "\n";
+            }
+        }
+
         if ($count === 0) {
             $settings = new Setting();
             $settings->site = $site;
-            $settings->ng_title = $request->input('ng_title');
-            $settings->ng_content = $request->input('ng_content');
+            $settings->ng_title = $title;
+            $settings->ng_content = $content;
             $result = $settings->save();
         } else {
             $settings = $setting->where('site', $site)->first();
-            $settings->ng_title = $request->input('ng_title');
-            $settings->ng_content = $request->input('ng_content');
+            $settings->ng_title = $title;
+            $settings->ng_content = $content;
             $result = $settings->update();
         }
         if ($result) {
