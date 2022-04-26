e454275fd950412850eca04c2c7ff60229546293
上田宏 <ueda.shoesaz@gmail.com>
2022-04-26 14:44:26 +0900

    bugfix:digimart set stock

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -27,8 +27,8 @@ class StocksController extends Controller
             ->where('en_brand', '!=', '')
             ->leftJoin("{$site}s", "{$site}s.id", '=', "{$site}_items.{$site}_id")
             ->select("{$site}_items.id", "{$site}_items.en_title", "{$site}_items.en_content")
+            ->limit(100)
             ->where("{$site}s.status", '=', 3)
-            ->where("limit", '=', 100)
             ->get();
 
         $result = '';
