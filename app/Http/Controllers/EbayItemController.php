f2d6f7d344b86c8bf74b83e276a0e7c8891f170b
上田宏 <ueda.shoesaz@gmail.com>
2022-05-02 06:28:29 +0900

    bugfix:secoundstreet出品

--- app/Http/Controllers/EbayItemController.php
+++ app/Http/Controllers/EbayItemController.php
@@ -35,7 +35,7 @@ class EbayItemController extends Controller
         'rakuten' => 'App\Models\Rakuten',
         'digimart' => 'App\Models\Digimarts',
         'hardoff' => 'App\Models\Hardoff',
-        'secoundstreet' => 'App\Models\secoundstreets',
+        'secoundstreet' => 'App\Models\Secoundstreets',
     ];
 
 
