82ba514e9e6774395e8e7e663ef0b30507a6da72
上田宏 <uedymd@uedahiroshinoMacBook-Pro.local>
2022-07-19 17:36:02 +0900

    bugfix:テキスト修正

--- resources/views/template/edit.blade.php
+++ resources/views/template/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            価格レート設定
+        テンプレート設定
         </h2>
     </x-slot>
 
