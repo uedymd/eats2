9932d612137efe8a3d4243788bc55bdeb72e9771
上田宏 <ueda.shoesaz@gmail.com>
2022-08-01 15:33:35 +0900

    bugfix:message file upload

--- public/js/app.js
+++ public/js/app.js
@@ -5313,7 +5313,7 @@ $(function () {
     }).done(function (result) {
       $("#loader").hide();
       console.log(result);
-      ret = JSON.parse(result);
+      var ret = JSON.parse(result);
 
       if (ret["Ack"] !== "Failure") {
         image = ret["SiteHostedPictureDetails"]["FullURL"];
--- resources/js/message.js
+++ resources/js/message.js
@@ -19,7 +19,7 @@ $(function () {
             .done(function (result) {
                 $("#loader").hide();
                 console.log(result);
-                ret = JSON.parse(result);
+                let ret = JSON.parse(result);
                 if (ret["Ack"] !== "Failure") {
                     image = ret["SiteHostedPictureDetails"]["FullURL"];
                     html = "";
